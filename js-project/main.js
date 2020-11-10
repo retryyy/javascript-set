03a4d7141459a4c2a77e0abe16cb226dcc6adce8
Valentin Somfalvai <valentin.somfalvai@ericsson.com>
2020-11-10 18:05:11 +0100

    First push

--- js-project/main.js
+++ js-project/main.js
@@ -1,5 +1,5 @@
 // scoreboard visibility
-let board = document.querySelector('.cards-board');
+let board = document.querySelector('#cards-board');
 let scoreboard = document.querySelector('#scoreboard');
 document.addEventListener("keydown", (event) => {
     if (event.keyCode === 83) {
@@ -18,7 +18,48 @@ document.addEventListener("keyup", (event) => {
 let cards = document.querySelectorAll('.card');
 function tick() {
     this.classList.toggle('active');
+    exchange();
 }
+
+function exchange() {
+    let tickedCards = document.querySelectorAll('.card.active');
+    if (tickedCards.length == 3) {
+        setTimeout(() => {
+            tickedCards.forEach(card => card.classList.add('fade'));
+            setTimeout(() => {
+                moveOut(tickedCards);
+                setTimeout(() => {
+                    moveBack(tickedCards);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }
+    console.log(tickedCards);
+}
+
+function moveOut(tickedCards) {
+    tickedCards.forEach(card => card.firstElementChild.src = "icons/2HrS.svg");
+    tickedCards.forEach(card => card.classList.remove('active'));
+    tickedCards.forEach(card => card.classList.add('new'));
+    tickedCards.forEach(card => card.classList.remove('fade'));
+}
+
+function moveBack(tickedCards) {
+    tickedCards.forEach(card => card.classList.add('trans'));
+    tickedCards.forEach(card => card.classList.remove('new'));
+    setTimeout(() => {
+        tickedCards.forEach(card => card.classList.remove('trans'));
+    }, 1500);
+}
+
 cards.forEach(card => card.addEventListener('click', tick));
 
 
+// sidebar
+let sidebarClick = document.querySelector('#sidebarClick');
+let sidebar = document.querySelector('#sidebar');
+sidebarClick.addEventListener('click', () => {
+    sidebarClick.classList.toggle('opened');
+    sidebar.classList.toggle('opened');
+    sidebarClick.firstElementChild.classList.toggle('click');
+})
