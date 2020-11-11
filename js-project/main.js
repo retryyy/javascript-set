c133d90d6017e7243ead081b56f0036bea593e64
Valentin Somfalvai <valentin.somfalvai@ericsson.com>
2020-11-11 18:59:39 +0100

    Scoreboard and effects

--- js-project/main.js
+++ js-project/main.js
@@ -1,8 +1,24 @@
-// scoreboard visibility
+// helper
+function sleep(ms) {
+    return new Promise((accept) => {
+        setTimeout(() => {
+            accept();
+        }, ms);
+    });
+}
+
+// DOM
+let sidebarClick = document.querySelector('#sidebarClick');
+let sidebar = document.querySelector('#sidebar');
+let logo = document.querySelector('#name');
 let board = document.querySelector('#cards-board');
 let scoreboard = document.querySelector('#scoreboard');
+let table = scoreboard.querySelector("table");
+let cards = document.querySelectorAll('.card');
+
+// scoreboard visibility
 document.addEventListener("keydown", (event) => {
-    if (event.keyCode === 83) {
+    if (event.keyCode === 83 && !sidebar.classList.contains('opened')) {
         scoreboard.classList.add('appear');
         board.classList.add('appear')
     }
@@ -14,27 +30,29 @@ document.addEventListener("keyup", (event) => {
     }
 });
 
+// scoreboard fill
+let players = 10;
+table.style.height = ["calc(100% *", players, "/ 10)"].join(" ");
+
 // cards
-let cards = document.querySelectorAll('.card');
 function tick() {
     this.classList.toggle('active');
     exchange();
 }
 
-function exchange() {
+async function exchange() {
     let tickedCards = document.querySelectorAll('.card.active');
     if (tickedCards.length == 3) {
-        setTimeout(() => {
-            tickedCards.forEach(card => card.classList.add('fade'));
-            setTimeout(() => {
-                moveOut(tickedCards);
-                setTimeout(() => {
-                    moveBack(tickedCards);
-                }, 1000);
-            }, 1000);
-        }, 1000);
+        board.classList.toggle('lock');
+        
+        await sleep(1000);
+        tickedCards.forEach(card => card.classList.add('fade'));
+        await sleep(1000);
+        moveOut(tickedCards);
+        await sleep(1000);
+        await moveBack(tickedCards);
+        board.classList.toggle('lock');
     }
-    console.log(tickedCards);
 }
 
 function moveOut(tickedCards) {
@@ -44,22 +62,22 @@ function moveOut(tickedCards) {
     tickedCards.forEach(card => card.classList.remove('fade'));
 }
 
-function moveBack(tickedCards) {
+async function moveBack(tickedCards) {
     tickedCards.forEach(card => card.classList.add('trans'));
     tickedCards.forEach(card => card.classList.remove('new'));
-    setTimeout(() => {
-        tickedCards.forEach(card => card.classList.remove('trans'));
-    }, 1500);
+    await sleep(1500);
+    tickedCards.forEach(card => card.classList.remove('trans'));
 }
 
 cards.forEach(card => card.addEventListener('click', tick));
 
-
 // sidebar
-let sidebarClick = document.querySelector('#sidebarClick');
-let sidebar = document.querySelector('#sidebar');
 sidebarClick.addEventListener('click', () => {
     sidebarClick.classList.toggle('opened');
     sidebar.classList.toggle('opened');
     sidebarClick.firstElementChild.classList.toggle('click');
+
+    board.classList.toggle('shrink');
+    board.classList.toggle('lock');
+    logo.classList.toggle('appear');
 })
