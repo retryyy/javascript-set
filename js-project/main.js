eae9d0a4d2089e77279d47975c00e0a6b0e1cc0a
Valentin Somfalvai <valentin.somfalvai@ericsson.com>
2020-11-12 15:07:44 +0100

    Welcome text and menu system

--- js-project/main.js
+++ js-project/main.js
@@ -8,23 +8,54 @@ function sleep(ms) {
 }
 
 // DOM
+let welcome = document.querySelector('#welcome');
 let sidebarClick = document.querySelector('#sidebarClick');
 let sidebar = document.querySelector('#sidebar');
+let menu = document.querySelector('#menu');
+let menuClose = menu.querySelector('.close');
+let submit = menu.querySelector('#submit');
 let logo = document.querySelector('#logo');
 let board = document.querySelector('#cards-board');
+let cards = document.querySelectorAll('.card');
 let scoreboard = document.querySelector('#scoreboard');
 let table = scoreboard.querySelector("table");
-let cards = document.querySelectorAll('.card');
+
+let gameOngoing = false;
+
+// load
+loadPage();
+async function loadPage() {
+    welcome.classList.add('appear');
+
+    let elements = document.body.getElementsByTagName('div');
+    console.log(elements);
+    for (elem of elements) {
+        elem.classList.add('hidden');
+    }
+    await sleep(0);
+
+    sidebarClick.classList.add('opened');
+    sidebar.classList.add('opened');
+    sidebarClick.firstElementChild.classList.add('click');
+    
+    menu.classList.remove('appear');
+    logo.classList.add('appear');
+    welcome.classList.remove('appear');
+    
+    for (elem of elements) {
+        elem.classList.remove('hidden');
+    }
+}
 
 // scoreboard visibility
 document.addEventListener("keydown", (event) => {
     if (event.keyCode === 83 && !sidebar.classList.contains('opened')) {
         scoreboard.classList.add('appear');
-        board.classList.add('appear')
+        board.classList.remove('appear')
     }
 });
 document.addEventListener("keyup", (event) => {
-    if (event.keyCode === 83) {
+    if (event.keyCode === 83 && !sidebar.classList.contains('opened')) {
         scoreboard.classList.remove('appear');
         board.classList.remove('appear');
     }
@@ -73,11 +104,45 @@ cards.forEach(card => card.addEventListener('click', tick));
 
 // sidebar
 sidebarClick.addEventListener('click', () => {
+    if (gameOngoing) {
+        sidebarClick.classList.toggle('opened');
+        sidebar.classList.toggle('opened');
+        sidebarClick.firstElementChild.classList.toggle('click');
+
+
+        if (sidebar.classList.contains('opened')) {
+            menu.classList.remove('appear');
+            board.classList.remove('appear');
+            logo.classList.add('appear');
+        } else {
+            board.classList.add('appear');
+            logo.classList.remove('appear');
+        }
+    }  
+})
+
+// menu
+sidebar.querySelector('#new').addEventListener('click', () => {
+    menu.classList.toggle('appear');
     sidebarClick.classList.toggle('opened');
     sidebar.classList.toggle('opened');
-    sidebarClick.firstElementChild.classList.toggle('click');
+    sidebarClick.firstElementChild.classList.remove('click');
 
-    board.classList.toggle('shrink');
-    board.classList.toggle('lock');
-    logo.classList.toggle('appear');
-})
+    board.classList.remove('appear');
+    logo.classList.remove('appear');
+});
+
+submit.addEventListener('click', () => {
+    menu.classList.remove('appear');
+    board.classList.add('appear');
+    gameOngoing = true;
+});
+
+menuClose.addEventListener('click', () => {
+    logo.classList.add('appear');
+
+    menu.classList.remove('appear');
+    sidebarClick.classList.add('opened');
+    sidebar.classList.add('opened');
+    sidebarClick.firstElementChild.classList.add('click');
+});
