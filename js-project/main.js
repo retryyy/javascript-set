9a0b78b9b04a7604cfe28a2f037d5062154823e3
Valentin Somfalvai <valentin.somfalvai@ericsson.com>
2020-11-22 18:37:49 +0100

    Shrinking board with less card

--- js-project/main.js
+++ js-project/main.js
@@ -24,6 +24,7 @@ let game = document.querySelector('#game');
 let layout = document.querySelector('#layout');
 let players = layout.querySelector('#players');
 let board = game.querySelector('#cards-board');
+let boardContainer = board.querySelector('#board');
 let cards = board.querySelectorAll('.card');
 let timer = game.querySelector('#timer');
 
@@ -33,6 +34,8 @@ let activePlayer;
 let nrOfPlayers = 1;
 let nrOfInstantiatedPlayers = 0;
 let playersInfo = [];
+
+let mode = "twenty-one";
 
 // load
 loadPage();
@@ -118,16 +121,11 @@ cards.forEach(card => card.addEventListener('click', () => {
 async function exchange() {
     let tickedCards = document.querySelectorAll('.card.active');
     if (tickedCards.length == 3) {
+        cards = board.querySelectorAll('.card');
+        await checkSet(tickedCards);
+        
         board.classList.remove('unlock');
         playersInfo[activePlayer].score += 1;
-        acted = true;
-        
-        await sleep(1000);
-        tickedCards.forEach(card => card.classList.add('fade'));
-        await sleep(1000);
-        await moveOut(tickedCards);
-        await sleep(1000);
-        await moveBack(tickedCards);
 
         startNewRound();
     }
@@ -152,11 +150,8 @@ async function startNewRound() {
     choosing = false;
 }
 
-function moveOut(tickedCards) {
+async function moveOut(tickedCards) {
     tickedCards.forEach(card => card.firstElementChild.src = "icons/2HrS.svg");
-    tickedCards.forEach(card => card.classList.remove('active'));
-    tickedCards.forEach(card => card.classList.remove('fade'));
-
     let bounds = board.getBoundingClientRect();
     let half = (bounds.top + bounds.bottom) / 2;
     
@@ -164,6 +159,8 @@ function moveOut(tickedCards) {
         let cHalf = (card.getBoundingClientRect().top + card.getBoundingClientRect().bottom) / 2;
         card.style.transform = `translate(${(half - cHalf)}px, ${document.documentElement.scrollWidth + board.getBoundingClientRect().width / 3}px) scale(3)`;
     });
+    await sleep(500);
+    tickedCards.forEach(card => card.classList.remove('fade'));
 }
 
 async function moveBack(tickedCards) {
@@ -186,7 +183,6 @@ sidebarClick.addEventListener('click', () => {
         sidebar.classList.toggle('opened');
         sidebarClick.firstElementChild.classList.toggle('click');
 
-
         if (sidebar.classList.contains('opened')) {
             menu.classList.remove('appear');
             layout.classList.remove('appear');
@@ -197,7 +193,6 @@ sidebarClick.addEventListener('click', () => {
         }
     }  
 });
-
 
 // menu
 sidebar.querySelector('#new').addEventListener('click', () => {
@@ -220,9 +215,7 @@ start.addEventListener('click', () => {
     playerName.classList.add('appear');
 });
 
-start.addEventListener('transitionend', () => {
-    playerInput.focus();
-});
+start.addEventListener('transitionend', () => playerInput.focus());
 
 //proceed
 proceed.addEventListener('click', () => {
@@ -302,10 +295,8 @@ for (i = 0; i < inc.length; i++) {
         min = incI.getAttribute("min"),
         max = incI.getAttribute("max"),
         step = incI.getAttribute("step");
-    document.getElementById(id)
-        .previousElementSibling.setAttribute('onclick', "stepperInput('" + id + "', -" + step + ", " + min + ")"); 
-    document.getElementById(id)
-        .nextElementSibling.setAttribute('onclick', "stepperInput('" + id + "', " + step + ", " + max + ")"); 
+    document.getElementById(id).previousElementSibling.setAttribute('onclick', "stepperInput('" + id + "', -" + step + ", " + min + ")"); 
+    document.getElementById(id).nextElementSibling.setAttribute('onclick', "stepperInput('" + id + "', " + step + ", " + max + ")"); 
 }
 
 function stepperInput(id, s, m) {
@@ -315,6 +306,172 @@ function stepperInput(id, s, m) {
     } else if (parseInt(el.value) > m) {
         el.value = parseInt(el.value) + s;
     }
+}
+
+async function checkSet(tickedCards) {
+    acted = true;
+    let arr = Array.from(cards);
+    let removed = [arr.indexOf(tickedCards[0]), arr.indexOf(tickedCards[1]), arr.indexOf(tickedCards[2])];
+
+    await sleep(1000);
+    tickedCards.forEach(card => {
+        card.classList.add('fade');
+        card.classList.remove('active');
+    });
+
+    if (mode == "") {
+        await sleep(1000);
+        await moveOut(tickedCards);
+        await sleep(500);
+        await moveBack(tickedCards);
+        return true;
+    }
+
+    console.log('lel');
+
+    await sleep(1000);
+    let fadings;
+    let oldMode = mode;
+    if (mode == 'twenty-one') {
+        fadings = [6, 13, 20];
+        mode = 'eighteen'
+    } else if (mode == 'eighteen') {
+        fadings = [5, 11, 17];
+        mode = 'fifteen'
+    } else if (mode == 'fifteen') {
+        fadings = [4, 9, 14];
+        mode = "";
+    }
+
+    let needAnimTo = removed.filter(card => !fadings.includes(card));
+    let needAnimFrom = fadings.filter(card => !removed.includes(card));
+        
+    for (let i = 0; i < needAnimFrom.length; i++) animation(needAnimFrom[i], needAnimTo[i]);
+
+    await sleep(2000);
+    needAnimTo.forEach(needTo => {
+        cards[needTo].classList.remove('fade');
+        cards[needTo].classList.remove('hide');
+    });
+
+    await sleep(500);
+    needAnimFrom.forEach(needFrom => cards[needFrom].style.opacity = 0);
+    board.querySelector('#board').classList.add('shrink');
+    board.classList.remove(oldMode);
+    if (mode != "") board.classList.add(mode);
+    cards.forEach(card => {
+        card.classList.remove(oldMode);
+        if (mode != "") card.classList.add(mode);
+    })
+    fadings.forEach(fade => cards[fade].style.width = '0%');
+
+    await sleep(500);
+    boardContainer.classList.add('transition-zero');
+    boardContainer.classList.remove('shrink');
+    fadings.forEach(fade => cards[fade].remove());
+
+    await sleep(500);
+    boardContainer.classList.remove('transition-zero');
+
+    return false;
+}
+
+async function animation(from, to) {
+    let cardFrom = cards[from];
+    let cardTo = cards[to];
+
+    cardFrom.firstElementChild.src = "icons/2HrS.svg";
+    cardTo.firstElementChild.src = "icons/2HrS.svg";
+    cardTo.classList.add('hide');
+
+    await sleep(1000);
+    let yB = cardFrom.getBoundingClientRect().right - cardTo.getBoundingClientRect().right;
+    let xB = cardTo.getBoundingClientRect().top - cardFrom.getBoundingClientRect().top;
+    cardFrom.style.transform = `translate(${xB}px, ${yB}px)`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function animation2() {
+    let cardA = cards[6]; // eighteen
+    cardA.style.opacity = 0;
+    let cardB = cards[13];
+    let cardC = cards[20];
+
+    let cardX = cards[1];
+    let cardY = cards[10];
+
+    cardB.firstElementChild.src = "icons/2HrS.svg";
+    cardC.firstElementChild.src = "icons/2HrS.svg";
+    cardX.firstElementChild.src = "icons/2HrS.svg";
+    cardY.firstElementChild.src = "icons/2HrS.svg";
+    cardX.style.opacity = 0;
+    cardY.style.opacity = 0;
+
+    await sleep(1000);
+    let yB = cardB.getBoundingClientRect().right - cardX.getBoundingClientRect().right;
+    let xB = cardX.getBoundingClientRect().top - cardB.getBoundingClientRect().top;
+    cardB.style.transform = `translate(${xB}px, ${yB}px)`;
+
+    let yC = cardC.getBoundingClientRect().right - cardY.getBoundingClientRect().right;
+    let xC = cardY.getBoundingClientRect().top - cardC.getBoundingClientRect().top;
+    cardC.style.transform = `translate(${xC}px, ${yC}px)`;
+    
+    await sleep(1000);
+
+    cardX.style.opacity = 1;
+    cardB.style.opacity = 0;
+    cardY.style.opacity = 1;
+    cardC.style.opacity = 0;
+    
+    await sleep(500);
+
+    board.querySelector('#board').classList.add('shrink');
+    
+    
+    
+    board.classList.remove('twenty-one');
+    board.classList.add('eighteen');
+
+    cards.forEach(card => {
+        card.classList.remove('twenty-one');
+        card.classList.add('eighteen');
+    })
+    cardA.style.width = '0%';
+    cardB.style.width = '0%';
+    cardC.style.width = '0%';
+
+    await sleep(500);
+    
+    board.querySelector('#board').classList.add('transition-zero');
+    board.querySelector('#board').classList.remove('shrink');
+    //
+    
+    cardA.remove();
+    cardB.remove();
+    cardC.remove();
+
+    await sleep(500);
+    board.querySelector('#board').classList.remove('transition-zero');
 }
 
 document.querySelector('#btnStart').addEventListener('click', async () => {
@@ -413,64 +570,5 @@ document.querySelector('#btnStart').addEventListener('click', async () => {
     cardB.remove();
     cardC.remove();*/
 
-    let cardA = cards[6]; // eighteen
-    cardA.style.opacity = 0;
-    let cardB = cards[13];
-    let cardC = cards[20];
-
-    let cardX = cards[1];
-    let cardY = cards[10];
-
-    cardB.firstElementChild.src = "icons/2HrS.svg";
-    cardC.firstElementChild.src = "icons/2HrS.svg";
-    cardX.firstElementChild.src = "icons/2HrS.svg";
-    cardY.firstElementChild.src = "icons/2HrS.svg";
-    cardX.style.opacity = 0;
-    cardY.style.opacity = 0;
-
-    await sleep(1000);
-    let yB = cardB.getBoundingClientRect().right - cardX.getBoundingClientRect().right;
-    let xB = cardX.getBoundingClientRect().top - cardB.getBoundingClientRect().top;
-    cardB.style.transform = `translate(${xB}px, ${yB}px)`;
-
-    let yC = cardC.getBoundingClientRect().right - cardY.getBoundingClientRect().right;
-    let xC = cardY.getBoundingClientRect().top - cardC.getBoundingClientRect().top;
-    cardC.style.transform = `translate(${xC}px, ${yC}px)`;
     
-    await sleep(1000);
-
-    cardX.style.opacity = 1;
-    cardB.style.opacity = 0;
-    cardY.style.opacity = 1;
-    cardC.style.opacity = 0;
-    
-    await sleep(500);
-
-    board.querySelector('#board').classList.add('shrink');
-    
-    
-    
-    board.classList.remove('twenty-one');
-    board.classList.add('eighteen');
-
-    cards.forEach(card => {
-        card.classList.remove('twenty-one');
-        card.classList.add('eighteen');
-    })
-    cardA.style.width = '0%';
-    cardB.style.width = '0%';
-    cardC.style.width = '0%';
-
-    await sleep(500);
-    
-    board.querySelector('#board').classList.add('transition-zero');
-    board.querySelector('#board').classList.remove('shrink');
-    //
-    
-    cardA.remove();
-    cardB.remove();
-    cardC.remove();
-
-    await sleep(500);
-    board.querySelector('#board').classList.remove('transition-zero');
 });
